80b7eb382063da2683ff9ddefadde1e7cd5bdb6b
unknown <sidheshthakur18@gmail.com>
2024-08-30 12:26:48 +0530

    Updated the first program

--- src/com/gitPrac/EmployeeWage.java
+++ src/com/gitPrac/EmployeeWage.java
@@ -1,10 +1,24 @@
 package com.gitPrac;
+import java.util.*;
 
 public class EmployeeWage {
+	
+	 public void isPresent(){
+		    Random random = new Random();       
+		    int attendance = random.nextInt(2);
+		    
+		        if (attendance == 0) {
+		          System.out.println("Employee is absent.");
+		         } else {
+		          System.out.println("Employee is present.");
+		      }
+		    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Welcome to Employee Wage Computation Program");
+        EmployeeWage ew1=new EmployeeWage();
+        ew1.isPresent();
 
 	}
 
