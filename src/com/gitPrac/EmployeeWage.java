e205ff0df996873601ff53e2dd4cdd81d83ff641
unknown <sidheshthakur18@gmail.com>
2024-08-30 11:58:00 +0530

    Created the master

---
+++ src/com/gitPrac/EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.gitPrac;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
